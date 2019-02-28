7eb5edec97c7fab191be334cd78b35308c943576
Chao Li <chao1.li@intel.com>
2019-02-28 15:14:21 +0800

    install openssh-server
    Signed-off-by: Chao Li <chao1.li@intel.com>

--- setup_main.sh
+++ setup_main.sh
@@ -113,7 +113,9 @@ if [ "$NETWORK_PROXY" == "1" ]; then
   echo $ROOT_PASSWD | sudo -S touch /etc/apt/apt.conf.d/10proxy
   echo $ROOT_PASSWD | sudo -S sh -c  'echo "Acquire::http::proxy \"http://child-prc.intel.com:913\";" > /etc/apt/apt.conf.d/10proxy'
   echo $ROOT_PASSWD | sudo -S cp $basedir/config/network/sudoers /etc/
+
   #Setup Intel Lab Scan Account
+  echo $ROOT_PASSWD | sudo -S apt-get install -y openssh-server
   echo $ROOT_PASSWD | sudo -S wget -4 -e "http_proxy=http://child-prc.intel.com:913" -q -O - http://isscorp.intel.com/IntelSM_BigFix/33570/package/scan/labscanaccount.sh | sudo -S bash -s --
 fi
 
